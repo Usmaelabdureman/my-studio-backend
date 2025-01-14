ff9354a4946f8886b348856e996c875092e8fd05
Usmaelabdureman <uabdureman@gmail.com>
2025-01-14 13:58:57 +0300

    added getting users

---
+++ src/app/module/User/User.swagger.ts
@@ -0,0 +1,52 @@
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: User management and authentication
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     summary: Retrieve a single user by ID
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The user ID
+ *     responses:
+ *       200:
+ *         description: A single user
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       404:
+ *         description: User not found
+ */
+
+/**
+ * User routes for the application.
+ */
